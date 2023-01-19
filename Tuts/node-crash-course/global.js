8f7086b4ffb8ee6f3d0f6887b6a17f9ffebfb328
x4n6u <joshuasmlaw@gmail.com>
2023-01-19 08:29:05 -0700

    node update

---
+++ Tuts/node-crash-course/global.js
@@ -0,0 +1,16 @@
+//Global object
+//console.log(global);
+
+// setTimeout(() => {
+//     console.log('int the timeout');
+//     clearInterval(int);
+// }, 3000);
+
+// const int = setInterval(() => {
+//     console.log('in the interval');
+// },1000);
+
+// console.log(__dirname);
+// console.log(__filename);
+
+console.log();
---
+++ Tuts/node-crash-course/people.js
@@ -0,0 +1,8 @@
+const people = ['yoshi', 'ryu', 'chun-l1', 'mario'];
+const ages = [20,25, 30, 35]
+
+//console.log(people);
+
+module.exports = {
+    people, ages
+};
