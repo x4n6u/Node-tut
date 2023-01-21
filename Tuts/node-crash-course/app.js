af34b93c665fb2126f07fc38c174cb70f9efeb4b
x4n6u <joshuasmlaw@gmail.com>
2023-01-21 04:08:28 -0700

    Video 9

--- Tuts/node-crash-course/app.js
+++ Tuts/node-crash-course/app.js
@@ -1,25 +1,44 @@
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
+
 
 // express app
 const app = express();
+
+//connect to mongodb
+const dbURI = 'mongodb+srv://netninja:test1234@nodetuts.t9nsuir.mongodb.net/node-tuts?retryWrites=true&w=majority';
+mongoose.set('strictQuery', true);
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(result => app.listen(3000))
+    .catch((err) => console.log(err));
 
 //register view engine
 app.set('view engine', 'ejs');
 
 //listen for requests
-app.listen(3000);
+//app.listen(3000);
+
+// middleware & static files
+app.use(express.static('public'));
+app.use(morgan('dev'));
+
+// routes
 
 app.get('/', (req, res) => {
 
     //res.send('<p>home page</p>');
     //res.sendFile('./views/index.html', {root: __dirname});
 
-    const blogs = [
-        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    ];
-    res.render('index', {title: 'Home',blogs});
+    // const blogs = [
+    //     {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    //     {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    //     {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    // ];
+    // res.render('index', {title: 'Home',blogs});
+
+    res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
@@ -28,6 +47,19 @@ app.get('/about', (req, res) => {
     //res.sendFile('./views/about.html', {root: __dirname});
     res.render('about', {title: 'About'});
 });
+
+// blog routes
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({ createdAt: -1})
+    .then((result) => {
+        res.render('index', {title: 'All Blogs', blogs: result})
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+});
+
+
 
 app.get('/blogs/create', (req, res) => {
     res.render('create', {title: 'Create a new Blog'});
