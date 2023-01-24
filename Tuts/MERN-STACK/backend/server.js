a0cfcf5132f2315cf50469b98d15b650016cd58a
x4n6u <joshuasmlaw@gmail.com>
2023-01-24 11:38:18 -0700

    Lesson 3
    Lesson 3 completed

--- Tuts/MERN-STACK/backend/server.js
+++ Tuts/MERN-STACK/backend/server.js
@@ -1,20 +1,22 @@
 require('dotenv').config();
 
 const express = require('express');
+const workoutRoute = require('./routes/workouts');
 
 //express app
 const app = express();
 
 //middleware
+app.use(express.json());
+
 app.use((req, res, next) => {
  console.log(req.path, req.method);
  next()
 });
 
 //routes
-app.get('/', (req, res) => {
-    res.json({mssg: 'Welcome to the app'})
-});
+app.use('/api/workouts', workoutRoute);
+
 //listen for request
 app.listen(process.env.PORT, () => {
     console.log('listening on port ', process.env.PORT );
